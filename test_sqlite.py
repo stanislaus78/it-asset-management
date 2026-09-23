55da2d5c40dd29cec27dbebc50dc43ce1cc25fee
Stanislav Vranesevic <vr.stanislav78@gmail.com>
2026-09-23 22:20:38 +0200

    Asset Freigabe hinzugefügt

---
+++ test_sqlite.py
@@ -0,0 +1,42 @@
+
+
+import sqlite3
+
+verbindung = sqlite3.connect("asset_management.db")
+
+cursor = verbindung.cursor()
+
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS assets (
+#     inventarnummer TEXT,
+#     hersteller TEXT,
+#     status TEXT,
+#     geraetetyp TEXT,
+#     standort TEXT,
+#     zimmer TEXT,
+#     organisationseinheit TEXT
+# )
+# """)
+
+# cursor.execute("""
+# INSERT INTO assets
+# VALUES (
+#     'LT001',
+#     'Dell',
+#     'Ausgegeben',
+#     'Laptop',
+#     'Zlb',
+#     '308',
+#     '602'
+# )
+# """)
+#cursor.execute("SELECT * FROM assets")
+cursor.execute("SELECT * FROM assets WHERE inventarnummer = 'LT001';")
+
+for asset in cursor.fetchall():
+    print(asset)
+verbindung.commit()
+
+print("Datensatz hinzugefügt")
+
+verbindung.close()
